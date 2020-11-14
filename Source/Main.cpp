3e8eafd4852501f6bba52a2e10810c4899986376
bladt <casperbladt@gmail.com>
2020-11-14 17:15:07 +0100

    .

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -19,13 +19,19 @@ struct RAM
 int main()
 {    
     using RAM_t = RAM<0x2000, 0x0000, 0x1FFF>;
+    using RAM2_t = RAM<0x2000, 0x2000, 0x3FFF>;
     RAM_t ram;
+    RAM2_t ramB;
 
-    Bus<uint16_t, uint8_t, RAM_t> bus(ram);
+    Bus<uint16_t, uint8_t, RAM_t, RAM2_t> bus(ram, ramB);
+    bus.Transmit(0x0000, 0x12);
+    bus.Transmit(0x0001, 0x13);
+    bus.Transmit(0x0002, 0x14);
+    bus.Transmit(0x0003, 0x15);
 
-    for (size_t n = RAM_t::AddressRangeStart; n < RAM_t::AddressRangeEnd; n++)
-    {
-        bus.Transmit(n, n);
-    }
+    bus.Transmit(0x2000, 0x12);
+    bus.Transmit(0x2001, 0x13);
+    bus.Transmit(0x2002, 0x14);
+    bus.Transmit(0x2003, 0x15);
     return 0;
 }
