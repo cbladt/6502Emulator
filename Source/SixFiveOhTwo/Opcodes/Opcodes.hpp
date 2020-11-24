cec1c424ec4f444d7e07bd18a8fb9d01c932b679
Casper Bladt <casper.bladt@bitzerdk.com>
2020-11-24 10:47:42 +0100

    dev

--- Source/SixFiveOhTwo/Opcodes/Opcodes.hpp
+++ Source/SixFiveOhTwo/Opcodes/Opcodes.hpp
@@ -20,33 +20,8 @@ namespace SixFiveOhTwo::Opcodes
                 break;
             }
 
-            case 0x0: BRK::Execute(cpu, ram); break;
-            case 0x1: ORA::Execute(cpu, ram); break;
-            case 0x5: ORA::Execute(cpu, ram); break;
-            case 0x6: ASL::Execute(cpu, ram); break;
-            case 0x8: PHP::Execute(cpu, ram); break;
-            case 0x9: ORA::Execute(cpu, ram); break;
-            case 0xA: ASL::Execute(cpu, ram); break;
-            case 0xD: ORA::Execute(cpu, ram); break;
-            case 0xE: ASL::Execute(cpu, ram); break;
-            case 0x10: BPL::Execute(cpu, ram); break;
-            case 0x11: ORA::Execute(cpu, ram); break;
-            case 0x15: ORA::Execute(cpu, ram); break;
-            case 0x16: ASL::Execute(cpu, ram); break;
-            case 0x18: CLC::Execute(cpu, ram); break;
-            case 0x19: ORA::Execute(cpu, ram); break;
-            case 0x1D: ORA::Execute(cpu, ram); break;
-            case 0x1E: ASL::Execute(cpu, ram); break;
-            case 0x20: JSR::Execute(cpu, ram); break;
-            case 0x21: AND::Execute(cpu, ram); break;
-            case 0x24: BIT::Execute(cpu, ram); break;
-            case 0x25: AND::Execute(cpu, ram); break;
-            case 0x26: ROL::Execute(cpu, ram); break;
-            case 0x28: PLP::Execute(cpu, ram); break;
-            case 0x29: AND::Execute(cpu, ram); break;
-            case 0x2A: ROL::Execute(cpu, ram); break;
-            case 0x2C: BIT::Execute(cpu, ram); break;
-            case 0x2D: AND::Execute(cpu, ram); break;
+            case 0x0: BRK::Execute(cpu, ram, 0);
+            case 0x1: ORA::Execute(cpu, ram, 6);
         }
     }
 }
